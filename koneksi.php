9c7035ee8f280be8bb36ef9769cb77037b76d403
mufti <mufutirf@gmail.com>
2026-02-02 11:43:02 +0700

    third commit

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,16 @@
 <?php
-$host = "localhost";
-$user = "admin";
-$pass = "Alika_itnsadei1012";
-$db   = "bsip_utama";
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
+$db   = $_ENV['DB_NAME'];
 
 $conn = mysqli_connect($host,$user,$pass,$db);
 ?>
