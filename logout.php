36c2d471f1c0fc66857f9c58698bfba5f3ce41b5
mufti <mufutirf@gmail.com>
2026-02-02 10:37:57 +0700

    second

--- logout.php
+++ logout.php
@@ -1,36 +1,34 @@
 <?php
 session_start();
 
-if(isset($_SESSION['pengguna'])) {
-    // User is logged in, proceed to logout
-    unset($_SESSION['pengguna']);
-};
+// User is logged in, proceed to logout
+unset($_SESSION['pengguna']);
 
-foreach ($_SESSION as $key => $value) {
-    unset($_SESSION[$key]);
-}
+// foreach ($_SESSION as $key => $value) {
+//     unset($_SESSION[$key]);
+// }
 
-$_SESSION = array();
+// $_SESSION = array();
 
-if(isset($_COOKIE[session_name()])){
-    setcookie(session_name(), '', time() - 3600, '/');
-};
+// if(isset($_COOKIE[session_name()])){
+//     setcookie(session_name(), '', time() - 3600, '/');
+// };
 
-$_SESSION = array();
+// $_SESSION = array();
 
-if(ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
-}
+// if(ini_get("session.use_cookies")) {
+//     $params = session_get_cookie_params();
+//     setcookie(session_name(), '', time() - 42000,
+//         $params["path"], $params["domain"],
+//         $params["secure"], $params["httponly"]
+//     );
+// }
 
 session_destroy();
 
-header("Cache-Control: no-cache, no-store, must-revalidate, max-age=0");
-header("Pragma: no-cache");
-header("Expires: Thu, 01 Jan 1970 00:00:00 GMT");
+// header("Cache-Control: no-cache, no-store, must-revalidate, max-age=0");
+// header("Pragma: no-cache");
+// header("Expires: Thu, 01 Jan 1970 00:00:00 GMT");
 
 header("Location: login.php");
 exit();
